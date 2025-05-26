2829f0797c1ac70fe225004a19d1be13b77adc35
hAbuMustafa <hosam5553@gmail.com>
2025-05-26 20:36:14 +0300

    rename hash function

--- encryption.py
+++ encryption.py
@@ -3,20 +3,20 @@ import base64
 import hashlib
 
 
-def pw_hash(password):
-    hashed = hashlib.sha256(password.encode()).digest()
+def hash(text):
+    hashed = hashlib.sha256(text.encode()).digest()
     encoded = base64.urlsafe_b64encode(hashed)
     return encoded.decode("utf-8")
 
 
 def encrypt_secrets(username, password, text_to_encrypt=""):
-    fernet = Fernet(pw_hash(password))
+    fernet = Fernet(hash(password))
     return fernet.encrypt(f"{username} {text_to_encrypt}".encode())
 
 
 def decrypt_secrets(username, password, encrypted_text):
     try:
-        fernet = Fernet(pw_hash(password))
+        fernet = Fernet(hash(password))
         decrypted = fernet.decrypt(encrypted_text).decode()
         segments = decrypted.split(" ")
         return segments[0] == username, segments[1:]
